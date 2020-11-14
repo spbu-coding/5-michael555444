c22f4aa4fa84ba0307fa9479164d98b325c0530a
michael555444 <33288939+michael555444@users.noreply.github.com>
2020-11-15 02:15:27 +0300

    5th hw solution (6th version)

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 void other() {
     printf("It's other function.\n"); fflush(stdout);
+    printf("Function had been returned to function <<other>>\n"); fflush(stdout);
 }
 
 void input() {
@@ -9,10 +10,12 @@ void input() {
     printf("Enter text:\n"); fflush(stdout);
     scanf("%s", buff);
     printf("%p", &other); fflush(stdout);
+    printf("Function stayed in function <<input>>\n"); fflush(stdout);
     printf("You enter: %s\n", buff); fflush(stdout);
 }
 
 int main() {
     input();
+    printf("Function had been returned to function <<main>>\n"); fflush(stdout);
     return 0;
 }
