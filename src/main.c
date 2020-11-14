3dc5a90505e3799f9498e88e2f207fb740487a65
michael555444 <33288939+michael555444@users.noreply.github.com>
2020-11-15 00:05:27 +0300

    5th hw solution

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define fflush(stdout) out
+
+void input() {
+    char buff[20];
+    ptintf("Enter text:\n"); out;
+    scanf("%s", buff);
+    printf("%p", &other); out;
+    printf("You enter: %s\n", buff); out;
+}
+
+void other(){
+    printf("It's other function.\n"); out;
+}
+
+int main(){
+    input();
+    return 0;
+}
