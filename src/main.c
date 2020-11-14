71b793f5f15561afbcfdc25e6d0332b2a515e54b
michael555444 <33288939+michael555444@users.noreply.github.com>
2020-11-15 00:16:25 +0300

    5th hw solution (5th version)

--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
+void other() {
+    printf("It's other function.\n"); fflush(stdout);
+}
+
 void input() {
     char buff[20];
     printf("Enter text:\n"); fflush(stdout);
     scanf("%s", buff);
     printf("%p", &other); fflush(stdout);
     printf("You enter: %s\n", buff); fflush(stdout);
-}
-
-void other() {
-    printf("It's other function.\n"); fflush(stdout);
 }
 
 int main() {
